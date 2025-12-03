31c68583336ab658b9e97043d224f5e80f6f6ab8
SURAJ KUMAR SHARMA <75290099+sharma62@users.noreply.github.com>
2025-12-03 14:03:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import ListContextProvider from "./context/listContext";
-import CmntContextProvider from "./context/CmntContext";
+import ListContextProvider from "./context/listContext.jsx";
+import CmntContextProvider from "./context/CmntContext.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ListContextProvider>
