18821d3a8454dcd7ba851028edb35e54fea26bb5
SURAJ KUMAR SHARMA <75290099+sharma62@users.noreply.github.com>
2025-12-03 14:28:59 +0530

    Fix import case for ListContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import ListContextProvider from "./context/listContext.jsx";
+import ListContextProvider from "./context/ListContext.jsx";
 import CmntContextProvider from "./context/CmntContext.jsx";
 
 createRoot(document.getElementById("root")).render(
